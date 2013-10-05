8ee1a43b5ada2f410c6e5d8f620accbf99a30967
michelc <michelc@gmail.com>
2013-10-05 17:40:05 +0200

    Suppression du package Daper.SimpleCRUD qui n'est pas utilisé.

--- Quick5/Models/Clients.cs
+++ Quick5/Models/Clients.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
 using System.Linq;
 using AutoMapper;
