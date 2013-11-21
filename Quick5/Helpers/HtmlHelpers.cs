4b19571e103410a03c0510e803d30cb37442128f
michelc <michelc@gmail.com>
2013-11-21 13:34:49 +0100

    Amélioration affichage dates et montants.

--- Quick5/Helpers/HtmlHelpers.cs
+++ Quick5/Helpers/HtmlHelpers.cs
@@ -12,8 +12,17 @@ namespace System.Web.Mvc
 
         public static List<T> AsList<T>(this T item)
         {
-            // new List<Quick5.Models.Siren>() { Model.Siren }
             return new List<T>() { item };
+        }
+
+        public static string ToShort(this DateTime? dt)
+        {
+            var value = dt.GetValueOrDefault();
+            if (value == DateTime.MinValue) return "";
+            if (value == DateTime.MaxValue) return "∞";
+            if (value == new DateTime(4712, 12, 31)) return "∞";
+
+            return value.ToString("dd/MM/yy");
         }
     }
 }
