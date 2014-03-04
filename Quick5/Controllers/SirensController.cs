c3a7b5c539a4da2cff60afe40810d0228e9043b6
michelc <michelc@gmail.com>
2014-03-04 16:15:21 +0100

    Interrogation de l'api pour les sirens.

--- Quick5/Controllers/SirensController.cs
+++ Quick5/Controllers/SirensController.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web.Mvc;
-using Quick5.Models;
 
 namespace Quick5.Controllers
 {
@@ -35,6 +39,54 @@ namespace Quick5.Controllers
             siren.Decisions = db.Decisions.List(siren.NSiren);
 
             return View(siren);
+        }
+
+        //
+        // GET: /Sirens/Api/5
+        // http://localhost:62636/Sirens/Api/44120
+
+        public ViewResult Api(int id)
+        {
+            var siren = db.Sirens.Get(id);
+
+            var prm = string.Format(ConfigurationManager.AppSettings.Get("api_prm"),
+                                    DateTime.Now.ToString("yyyMMdd"), 
+                                    DateTime.Now.ToString("HHmm"));
+            var md5 = ConvertMD5(prm + ConfigurationManager.AppSettings.Get("api_mdp"));
+            var url = string.Format(ConfigurationManager.AppSettings.Get("api_url"),
+                                    prm,
+                                    md5,
+                                    siren.NSiren);
+
+            string text = "";
+            try
+            {
+                var request = (HttpWebRequest)HttpWebRequest.Create(url);
+                var response = (HttpWebResponse)request.GetResponse();
+                var stream = response.GetResponseStream();
+                var reader = new StreamReader(stream);
+                text = reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                text = ex.Message;
+            }
+
+            ViewBag.Url = url;
+            ViewBag.Text = text;
+            return View(siren);
+        }
+
+        public string ConvertMD5(string value)
+        {
+            var md = MD5.Create();
+            var data = md.ComputeHash(Encoding.Default.GetBytes(value));
+            var hash = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                hash.Append(data[i].ToString("x2"));
+            }
+            return hash.ToString().ToLower();
         }
     }
 }
